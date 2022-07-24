223bd65dfcf26cc53ebfbcb7c3e4f68c7212d4e3
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2022-07-24 14:28:18 +0300

    fix: исправить ошибки после автотестов

--- routes/cards.js
+++ routes/cards.js
@@ -13,17 +13,17 @@ router.post('/cards', celebrate({
 }), createCard);
 router.delete(
   '/cards/:cardId',
-  celebrate({ params: Joi.string().min(24).max(24) }),
+  celebrate({ params: Joi.object().keys({ cardId: Joi.string().min(24).max(24) }) }),
   deleteCard,
 );
 router.put(
   '/cards/:cardId/likes',
-  celebrate({ params: Joi.string().min(24).max(24) }),
+  celebrate({ params: Joi.object().keys({ cardId: Joi.string().min(24).max(24) }) }),
   putLikeCard,
 );
 router.delete(
   '/cards/:cardId/likes',
-  celebrate({ params: Joi.string().min(24).max(24) }),
+  celebrate({ params: Joi.object().keys({ cardId: Joi.string().min(24).max(24) }) }),
   deleteLikeCard,
 );
 
